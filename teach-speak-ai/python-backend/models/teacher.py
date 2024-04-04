bc8e26df1277007e4e993dcd25f9f9444256d115
Foxdapple <110433393+Foxdapple@users.noreply.github.com>
2024-04-04 19:39:54 +1300

    Update teacher.py
    Wrote get functions for teacher model.

--- teach-speak-ai/python-backend/models/teacher.py
+++ teach-speak-ai/python-backend/models/teacher.py
@@ -2,26 +2,31 @@ import account
 import school
 
 class teacher:
-    def __init__(self):
-        pass
+    def __init__(self, firstname, lastname, school, account, class_name):
+        self.__id = None
+        self.__firstname = firstname
+        self.__lastname = lastname
+        self.__school = school
+        self.__account = account
+        self.__class_name = class_name
     
     def get_id(self) -> int:
-        pass
+        return self.__id
     
     def get_firstname(self) -> str:
-        pass
+        return self.__firstname
     
     def get_lastname(self) -> str:
-        pass
+        return self.__lastname
     
     def get_school(self) -> school:
-        pass
+        return self.__school
     
-    def get_accound(self) -> account:
-        pass
+    def get_account(self) -> account:
+        return self.__account
     
-    def get_class(self) -> str:
-        pass
+    def get_class_name(self) -> str:
+        return self.__class_name
     
     def get_json(self):
         pass
