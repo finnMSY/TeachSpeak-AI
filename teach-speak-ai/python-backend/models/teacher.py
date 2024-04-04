0a1d540639b0869de8ee980290fcdca222b737b8
Foxdapple <110433393+Foxdapple@users.noreply.github.com>
2024-04-04 19:27:16 +1300

    Update teacher.py
    Set up the skeleton for teacher.

---
+++ teach-speak-ai/python-backend/models/teacher.py
@@ -0,0 +1,25 @@
+import account
+import school
+
+class teacher:
+    def __init__(self):
+        pass
+    
+    def get_id(self) -> int:
+        pass
+    
+    def get_firstname(self) -> str:
+        pass
+    
+    def get_lastname(self) -> str:
+        pass
+    
+    def get_school(self) -> school:
+        pass
+    
+    def get_accound(self) -> account:
+        pass
+    
+    def get_class(self) -> str:
+        pass
+    
