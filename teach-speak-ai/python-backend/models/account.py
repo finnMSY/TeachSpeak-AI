5358a59175e871b6a46046cbd34b59ecc374f006
Foxdapple <110433393+Foxdapple@users.noreply.github.com>
2024-04-04 19:30:16 +1300

    Changed account.py
    added skeleton code to account model.

---
+++ teach-speak-ai/python-backend/models/account.py
@@ -0,0 +1,12 @@
+class account:
+    def __init__(self):
+        pass
+    
+    def get_username(self):
+        pass
+    
+    def get_password(self):
+        pass
+    
+    def get_account_type(self):
+        pass
