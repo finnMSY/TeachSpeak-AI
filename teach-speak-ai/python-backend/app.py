ca719b7a5ea86c48fee11463757b680388102f5e
OliverW <oliverw4321@gmail.com>
2024-04-15 19:16:33 +1200

    Changed app.py to prep for basic aws deploy
    Recorder util is incompatible with linux os so commented out for time being

--- teach-speak-ai/python-backend/app.py
+++ teach-speak-ai/python-backend/app.py
@@ -1,22 +1,22 @@
 from flask import Flask
-from Recorder import Recorder
+# from util.Recorder import Recorder
 from flask_cors import CORS
 
 app = Flask(__name__)
-recorder = Recorder()
+# recorder = Recorder()
 CORS(app)
 
 @app.route('/')
 def index():
     return 'Hello, world!'
 
-@app.route('/api/start_recording')
-def start_recording_api():
-    recorder.start_recording()
+# @app.route('/api/start_recording')
+# def start_recording_api():
+#     recorder.start_recording()
 
-@app.route('/api/stop_recording')
-def stop_recording_api():
-    recorder.stop_recording()
+# @app.route('/api/stop_recording')
+# def stop_recording_api():
+#     recorder.stop_recording()
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, host='0.0.0.0')
